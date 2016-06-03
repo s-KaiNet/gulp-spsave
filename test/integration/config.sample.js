e7a02fa9177e1c249099115081b858d42e768541
sergei_sergeev <sergeev.srg@gmail.com>
2016-06-03 08:51:17 +0300

    Fixed sample config.

--- test/integration/config.sample.js
+++ test/integration/config.sample.js
@@ -1,19 +1,19 @@
 
-export var onprem = {
+exports.onprem = {
   username: '[username]',
   password: '[password]'
 };
 
-export var env = {
+exports.env = {
   domain: '[domain]'
 };
 
-export var online = {
+exports.online = {
   username: '[username]',
   password: '[password]'
 };
 
-export var url = {
+exports.url = {
   online: 'https://[tenant].sharepoint.com/',
   onprem: 'http://sp2013dev/sites/dev/'
 };
